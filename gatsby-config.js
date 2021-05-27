7979d00e3e38398c07b0c1a9008fe224f1572fbe
pewriebontal <pewriebontal@gmail.com>
2021-05-27 17:10:56 +0000

    add offline function

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,68 @@
 module.exports = {
-    siteMetadata: {
-        title: `Blog | pewriebontal`,
-        name: `Blog | pewriebontal`,
-        siteUrl: `https://blog.pewriebontal.ml`,
-        description: `pewriebontal's personal blog.`,
-        hero: {
-            heading: `Welcome to Blog, pewriebontal's personal space.`,
-            maxWidth: 652,
-        },
-        social: [
-            {
-                name: `twitter`,
-                url: `https://twitter.com/pewriebontal`,
-            },
-            {
-                name: `github`,
-                url: `https://github.com/pewriebontal`,
-            },
-            {
-                name: `instagram`,
-                url: `https://instagram.com/pewriebontal`,
-            },
-            {
-                name: `linkedin`,
-                url: `https://linkedin.com/in/pewriebontal/`,
-            },
-            /* {
+  siteMetadata: {
+    title: `Blog | pewriebontal`,
+    name: `Blog | pewriebontal`,
+    siteUrl: `https://blog.pewriebontal.ml`,
+    description: `pewriebontal's personal blog.`,
+    twitterUsername: "@pewriebontal",
+    keywords: "pewrie bontal, Min Thu Khine, pewriebontal",
+    hero: {
+      heading: `Welcome to Blog, pewriebontal's personal space.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/pewriebontal`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/pewriebontal`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/pewriebontal`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://linkedin.com/in/pewriebontal/`,
+      },
+      /* {
                name: `dribbble`,
                url: `https://dribbble.com/pewriebontal`,
              }, */
-        ],
-    },
-    plugins: [
-        {
-            resolve: "@narative/gatsby-theme-novela",
-            options: {
-                contentPosts: "content/posts",
-                contentAuthors: "content/authors",
-                basePath: "/",
-                authorsPage: true,
-                sources: {
-                    local: true,
-                    // contentful: true,
-                },
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Blog | pewriebontal`,
-                short_name: `Blog`,
-                start_url: `/`,
-                background_color: `#fff`,
-                theme_color: `#fff`,
-                display: `standalone`,
-                icon: `src/assets/favicon.png`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-netlify-cms`,
-            options: {
-            },
-        },
     ],
+  },
+  plugins: [
+    `gatsby-plugin-offline`,
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Blog | pewriebontal`,
+        short_name: `Blog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {},
+    },
+  ],
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,16 +6,16 @@ import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
 import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
 
 function NotFoundPage() {
-    return (
-        <Layout>
-            <SEO />
-            <Section>
-                <div style={{ marginTop: "100px" }}>
-                    <Headings.h1>404: Page Not Found</Headings.h1>
-                </div>
-            </Section>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SEO />
+      <Section>
+        <div style={{ marginTop: "100px" }}>
+          <Headings.h1>404: Page Not Found</Headings.h1>
+        </div>
+      </Section>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
